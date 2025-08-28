79352799cdcac36e4be64a9cb73a42d05dde55f1
Yogesh Manivannan <you@example.com>
2025-08-28 19:46:47 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Intro from "./pages/intro";
+import AI from "./pages/ai";
+
+function App() {
+  return (
+    <Routes>
+    </Routes>
+  );
+}
+
+export default App;
